d9737c5414d9bdcabfd09373fe32d7271ed228e2
WinnieWei0 <1344228500@qq.com>
2018-08-22 11:51:15 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -80,6 +80,9 @@
         </a>
       </li>
     </ul>
+  </div> -->
+  <div class="example">
+
   </div>
 </template>
 
