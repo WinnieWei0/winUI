8d2998656b2a20fee66ce55b79457090582e2250
WinnieWei0 <1344228500@qq.com>
2018-09-06 17:59:06 +0800

    单选框绑定值

--- example/pages/radio.vue
+++ example/pages/radio.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-<w-radio name="check">男生</w-radio>
-<w-radio name="check">女生</w-radio>
-<w-radio name="check">
+<!-- <w-radio :value="value" label="1" @change="handleChange">男生</w-radio>
+<w-radio :value="value" label="2" @change="handleChange">女生</w-radio>
+<w-radio :value="value" label="3" @change="handleChange">
+  <img src="../../src/assets/logo.png" alt="">
+</w-radio> -->
+
+<w-radio v-model="value" label="1" @change="handleChange">男生</w-radio>
+<w-radio v-model="value" label="2" @change="handleChange">女生</w-radio>
+<w-radio v-model="value" label="3" @change="handleChange">
   <img src="../../src/assets/logo.png" alt="">
 </w-radio>
   </div>
@@ -12,11 +18,20 @@
 export default {
   data() {
     return {
-      check: 'a'
+      value: '1'
+    }
+  },
+  methods: {
+    handleChange(val) {
+      console.log(333, val, this.value)
     }
   }
 };
 </script>
 
 <style scoped>
+img{
+  width: 50px;
+  height: 50px;
+}
 </style>
--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -1,22 +1,67 @@
 <template>
 	<div>
+		<!-- <div class="w-radio-item">
+			<input type="radio" class="w-radio" :checked="checkVal" :value="label" :name='name' @input="handleChange($event.target.value)">
+			<span class="w-radioInput"></span>{{label}}
+			<slot></slot>
+		</div> -->
 		<div class="w-radio-item">
-			<input type="radio" class="w-radio" :name='name'>
-			<span class="w-radioInput"></span>
+			<input type="radio" class="w-radio" v-model="value" :value="label" :name='name' @change="handleChange">
+			<span class="w-radioInput"></span>{{label}}
 			<slot></slot>
 		</div>
-		<!-- <div class="w-radio-item">
-			<input type="radio" class="w-radio" name="a">
-			<span class="w-radioInput"></span>女生
-		</div> -->
 	</div>
 </template>
 
 <script>
 export default {
   name: 'w-radio',
+  data() {
+    return {
+      emitVal: ''
+    }
+  },
+  model: {
+    prop: 'modeVal',
+    event: 'change'
+  },
   props: {
-    name: String
+    name: {
+      type: String,
+      default: 'a'
+    },
+    modeVal: String,
+    // value: {
+    //   type: String,
+    //   default: ''
+    // },
+    // 'v-model': String,
+    label: String
+  },
+  computed: {
+    value: {
+      get() {
+        // console.log(444, val)
+        return this.modeVal
+      },
+      set(val) {
+        console.log(222, val, this.value, this.modeVal)
+        // this.value = val
+        // this.modeVal = val
+        // this.emitVal = val
+        this.$emit('input', val)
+      }
+    }
+    // value() {
+    //   this.$emit('change', this.value)
+    // }
+  },
+  methods: {
+    handleChange() {
+      console.log(111, this.value, this.modeVal)
+      // this.$emit('change', val)
+      this.$emit('change', this.value)
+    }
   }
 }
 </script>
