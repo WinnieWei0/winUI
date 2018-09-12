d429a16512affba357078c457781a2a356fae772
WinnieWei0 <1344228500@qq.com>
2018-09-12 21:10:45 +0800

    单选按钮完成

--- example/pages/radio.vue
+++ example/pages/radio.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
-<!-- <w-radio :value="value" label="1" @change="handleChange">男生</w-radio>
-<w-radio :value="value" label="2" @change="handleChange">女生</w-radio>
-<w-radio :value="value" label="3" @change="handleChange">
-  <img src="../../src/assets/logo.png" alt="">
-</w-radio> -->
-
-<w-radio v-model="value" label="1" @change="handleChange">男生</w-radio>
-<w-radio v-model="value" label="2" @change="handleChange">女生</w-radio>
-<w-radio v-model="value" label="3" @change="handleChange">
-  <img src="../../src/assets/logo.png" alt="">
-</w-radio>
+    <w-radio v-model="value" label="1">男生</w-radio>
+    <w-radio v-model="value" label="2">女生</w-radio>
+    <w-radio v-model="value" label="3">
+      <img src="../../src/assets/logo.png" alt="">
+    </w-radio>
+    <w-radio v-model="value" label="4" disabled>nonono</w-radio>
   </div>
 </template>
 
@@ -18,19 +13,14 @@
 export default {
   data() {
     return {
-      value: '1'
-    }
-  },
-  methods: {
-    handleChange(val) {
-      console.log(333, val, this.value)
-    }
+      value: "1"
+    };
   }
 };
 </script>
 
 <style scoped>
-img{
+img {
   width: 50px;
   height: 50px;
 }
--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -1,13 +1,8 @@
 <template>
 	<div>
-		<!-- <div class="w-radio-item">
-			<input type="radio" class="w-radio" :checked="checkVal" :value="label" :name='name' @input="handleChange($event.target.value)">
-			<span class="w-radioInput"></span>{{label}}
-			<slot></slot>
-		</div> -->
 		<div class="w-radio-item">
-			<input type="radio" class="w-radio" v-model="value" :value="label" :name='name' @change="handleChange">
-			<span class="w-radioInput"></span>{{label}}
+			<input type="radio" class="w-radio" :disabled="disabled" v-model="value" :value="label" :name='name'>
+			<span class="w-radioInput"></span>
 			<slot></slot>
 		</div>
 	</div>
@@ -31,36 +26,17 @@ export default {
       default: 'a'
     },
     modeVal: String,
-    // value: {
-    //   type: String,
-    //   default: ''
-    // },
-    // 'v-model': String,
-    label: String
+    label: String,
+    disabled: Boolean
   },
   computed: {
     value: {
       get() {
-        // console.log(444, val)
         return this.modeVal
       },
       set(val) {
-        console.log(222, val, this.value, this.modeVal)
-        // this.value = val
-        // this.modeVal = val
-        // this.emitVal = val
-        this.$emit('input', val)
+        this.$emit('change', val)
       }
-    }
-    // value() {
-    //   this.$emit('change', this.value)
-    // }
-  },
-  methods: {
-    handleChange() {
-      console.log(111, this.value, this.modeVal)
-      // this.$emit('change', val)
-      this.$emit('change', this.value)
     }
   }
 }
